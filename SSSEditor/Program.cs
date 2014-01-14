4b636ab54a4d7754041827b3fa703473857939a6
libertyernie <libertyernie0@gmail.com>
2014-01-14 15:16:49 -0600

    Add "minusery" to search paths

--- SSSEditor/Program.cs
+++ SSSEditor/Program.cs
@@ -26,6 +26,7 @@ namespace SSSEditor {
 			string pac = args.Length > 1 ? args[1]
 				: File.Exists(@"private\wii\app\RSBE\pf\system\common5.pac") ? @"private\wii\app\RSBE\pf\system\common5.pac"
 				: File.Exists(@"projectm\pf\system\common5.pac") ? @"projectm\pf\system\common5.pac"
+				: File.Exists(@"minusery\pf\system\common5.pac") ? @"minusery\pf\system\common5.pac"
 				: null;
 
 			if (gct == null) using (var dialog = new OpenFileDialog()) {
